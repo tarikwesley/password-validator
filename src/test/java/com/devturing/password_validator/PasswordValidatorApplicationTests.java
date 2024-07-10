02205d1a39428a02579c30500d51f81d44d27bbd
Tarik Wesley <tarikwesley@gmail.com>
2024-07-10 20:36:19 -0300

    feat: first commit

---
+++ src/test/java/com/devturing/password_validator/PasswordValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devturing.password_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
